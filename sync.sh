af0f1a8bec220b2c81c7b2a9e583a6c2f614c588
dneumann <dneumann@mhighpoint.com>
2022-03-22 15:34:35 -0700

    Created sync script that creates dirs

---
+++ sync.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Creating the directories
+
+mkdir -p $HOME/.local
+mkdir -p $HOME/.local/scripts
+mkdir -p $HOME/.local/docs
+
+mkdir -p $HOME/.repos
+mkdir -p $HOME/.repos/work
+mkdir -p $HOME/.repos/projects
+mkdir -p $HOME/.repos/random
+
+# Installing the needed packages
+
+
